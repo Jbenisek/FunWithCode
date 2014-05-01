f3521aff920e736613cee8bbce667fff4f6d537a
jbenisek <jb@dontpanic.biz>
2014-05-01 16:37:05 -0600

    Added functions with HoursXPay....

---
+++ FunWithCode/HoursXPayWithOverTimeAndFunctions.py
@@ -0,0 +1,31 @@
+hours = 0.0
+rate = 0.0
+normalpay = 0.0
+overtime = 0.0
+totalpay = 0.0
+
+def pay(normalpay, totalpay):
+    if hoursfloated <= 40:
+        normalpay = hoursfloated * ratefloated
+        return normalpay
+    else:
+        # Over time starts at 41
+        normalpay = 40 * ratefloated
+        overtime = ((hoursfloated - 40) * (ratefloated * 1.5))
+        totalpay = overtime + normalpay
+        return totalpay
+
+def tryfloat(x):
+    try:
+        x = float(x)
+        return (x)
+    except:
+        print "Failed to Float, please check entry and retry, Error= hours = float(hours)"
+    
+
+hours = raw_input("Enter Hours:")
+hoursfloated = tryfloat(hours)
+rate = raw_input("Enter Rate:")
+ratefloated = tryfloat(rate)
+
+print pay(normalpay, totalpay)
